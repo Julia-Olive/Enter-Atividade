9d5527d133c22b1ca67a237ebe4a2b274b82089d
Julia-Olive <juoli482@gmail.com>
2023-11-09 14:20:07 -0300

    feat: commit inicial

---
+++ Projetos/Atividade/js/01.js
@@ -0,0 +1,8 @@
+var number = 5
+if (number > 0) {
+    console.log("Positivo")
+} else if (number < 0) {
+    console.log("Negativo")
+} else {
+    console.log("Zero")
+}
---
+++ Projetos/Atividade/js/index.js
@@ -0,0 +1,7 @@
+var person = "João"
+var age = 20
+var ocupacao = "estudante" 
+
+console.log(typeof person)
+console.log(typeof age)
+console.log(typeof ocupacao)
